530161abc952a72f0e31dc072cbc87e2d693d20b
MugTree <waterhose@protonmail.com>
2024-05-09 13:02:00 +0100

    wip handlers

--- poker/main.go
+++ poker/main.go
@@ -10,7 +10,13 @@ func HandlePoker(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to poker!")
 }
 
+func HandleNotFound(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
+}
+
 func main() {
 	http.HandleFunc("/", HandlePoker)
+	http.HandleFunc("/*", HandleNotFound)
+	http.HandleFunc("/404", HandleNotFound)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
--- spins/main.go
+++ spins/main.go
@@ -10,7 +10,13 @@ func HandleSpins(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to spins!")
 }
 
+func HandleNotFound(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
+}
+
 func main() {
 	http.HandleFunc("/", HandleSpins)
+	http.HandleFunc("/*", HandleNotFound)
+	http.HandleFunc("/404", HandleNotFound)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
